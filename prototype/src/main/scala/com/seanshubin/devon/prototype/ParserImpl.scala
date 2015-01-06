1ffbc223e8011c4021ba4e0246b52044287ceed5
SeanShubin <seanshubin@gmail.com>
2015-01-05 23:44:37 -0800

    Renamed to avoid shadowing

--- prototype/src/main/scala/com/seanshubin/devon/prototype/ParserImpl.scala
+++ prototype/src/main/scala/com/seanshubin/devon/prototype/ParserImpl.scala
@@ -1,13 +1,12 @@
 package com.seanshubin.devon.prototype
 
 class ParserImpl[A,B](ruleLookup:RuleLookup[A,B]) extends Parser[A,B] {
-  def parse(ruleName:String, cursor:Cursor[A], assembler:Assembler[A, B]):Either[String, B] = {
-    val rule:Rule[A,B] = ruleLookup.lookupRuleByName(ruleName)
-    val matchResult:MatchResult[A,B] = rule.apply(ruleLookup, cursor, assembler)
+  def parse(originalRuleName:String, cursor:Cursor[A], originalAssembler:Assembler[A, B]):Either[String, B] = {
+    val rule:Rule[A,B] = ruleLookup.lookupRuleByName(originalRuleName)
+    val matchResult:MatchResult[A,B] = rule.apply(ruleLookup, cursor, originalAssembler)
     matchResult match {
       case MatchSuccess(ruleName, start, end, assembler) => Right(assembler.top)
       case MatchFail(ruleName, message) => Left(message)
-
     }
   }
 }
