55ac8f83973895c3463ae978cfc897eb98ff9773
SeanShubin <sshubin@cj.com>
2015-04-16 10:18:51 -0700

    Simple merge

---
+++ core/src/test/scala/com/seanshubin/devon/core/DevonTest.scala
@@ -0,0 +1,30 @@
+package com.seanshubin.devon.core
+
+import com.seanshubin.devon.core.devon.{Devon, DevonMarshallerWiring}
+import org.scalatest.FunSuite
+
+class DevonTest extends FunSuite {
+  test("simple merge") {
+    val left =
+      """{
+        |  read b
+        |  update c
+        |  delete d
+        |}
+      """.stripMargin
+    val right =
+      """{
+        |  create a
+        |  update d
+        |  delete ()
+        |}
+      """.stripMargin
+    val expected = "{read b create a update d delete()}"
+    val devonMarshaller = DevonMarshallerWiring.Default
+    val leftDevon = devonMarshaller.fromString(left)
+    val rightDevon = devonMarshaller.fromString(right)
+    val mergedDevon = Devon.merge(leftDevon, rightDevon)
+    val actual = devonMarshaller.toCompact(mergedDevon)
+    assert(actual === expected)
+  }
+}
