cbf97760bbd96ccc3bd22145b55049a03faa4824
Sean Shubin <sshubin@cj.com>
2016-09-21 15:44:04 -0700

    Moved passwords to environment variables

--- stage.sh
+++ stage.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
+# halt the script if we encounter any errors
+set -e
+
+# make sure we don't inherit any state from our local repository
+rm -rf ~/.m2/repository/com/seanshubin/devon/
+
+# make sure we don't inherit any state from previous runs
 mvn clean
-mvn deploy -P stage --settings=deploy-to-maven-central-settings.xml -Dgpg.keyname=B45DA2B9 -Dgpg.passphrase=***
+
+# deploy with the staging profile, depends on environment variables GPG_KEY_NAME, GPG_KEY_PASSWORD, and MAVEN_STAGING_PASSWORD
+mvn deploy -P stage --settings=deploy-to-maven-central-settings.xml -Dgpg.keyname=${GPG_KEY_NAME} -Dgpg.passphrase=${GPG_KEY_PASSWORD}
+
+# all done, emit a link describing where to check the results
+echo artifacts staged, see https://oss.sonatype.org/#stagingRepositories
